4f11917640a9f47923b6dfcc865493fda9a83de2
Gwxnbleidd <Dima_russla@mail.ru>
2024-11-02 03:20:02 +0300

    Вроде выполнил 4 ЛР

--- app/GUI/secret_phrase.py
+++ app/GUI/secret_phrase.py
@@ -1,11 +1,16 @@
+import time
 import tkinter as tk
 from tkinter import ttk
+import os
 
 from app.GUI.main_window import login_window
+from app.utils.encryption import generate_key_using_phrase, form_decrypt_file, encrypt_file
+from app.utils.database import User, Database
 
 def open_window():
 
     # Создание окна
+    global window
     window = tk.Tk()
     window.title("Ввод парольной фразы")
     window.geometry("300x200+800+300")
@@ -14,13 +19,13 @@ def open_window():
     phrase_label = ttk.Label(window, text="Введите парольную фразу:")
 
     # Поля ввода
-    global username_entry
     global result_label
+    global phrase_entry
     phrase_entry = ttk.Entry(window)
 
     # Кнопки
-    confirm_button = ttk.Button(window, text="ОК", command=login_window)
-    exit_button = ttk.Button(window, text="Отмена", command=window.destroy)
+    confirm_button = ttk.Button(window, text="ОК", command=click_processing)
+    exit_button = ttk.Button(window, text="Отмена", command=exit)
 
     # Текст результата
     result_label = ttk.Label(window, text="")
@@ -35,3 +40,42 @@ def open_window():
     result_label.place(x=60, y=160, width=250)
 
     window.mainloop()
+
+def click_processing():
+    # если файл не зашифрован - добавить пользователя и зашифровать
+    # если зашифрован - расшифровать
+    result_label['text'] = ''
+
+    global key
+    phrase = phrase_entry.get()
+    
+    if not os.path.exists('database.txt'):
+        admin = User(username='admin', password='')
+        database = Database('database.txt')
+        database.add_user(admin)
+        salt = os.urandom(16)
+        with open('salt.txt', 'wb') as file:
+            file.write(salt)
+        key = generate_key_using_phrase(phrase, salt)    
+        encrypt_file(key, 'database.txt', 'database.txt')
+    else:
+        with open('salt.txt', 'rb') as file:
+            salt = file.read()
+        key = generate_key_using_phrase(phrase, salt)
+
+    try:
+        form_decrypt_file(key)
+    except ValueError:
+        result_label['text'] = 'Неверная парольная фраза'
+        return
+
+    db = Database()
+    database = db.read()
+    print(database)
+    if database.get('admin'):
+        window.destroy()
+        login_window()
+    else:
+        result_label['text'] = 'Неверная парольная фраза'
+        exit()
+
--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 import tkinter as tk
 
-from app.utils.database import User, Database
 from app.GUI.secret_phrase import open_window as secret_phrase_window
 
-
-database = Database()
-
-admin = User(username='admin', password='')
-try:
-    database.add_user(admin)
-except Exception as e:
-    pass
-
 if __name__ == '__main__':
+
     secret_phrase_window()
